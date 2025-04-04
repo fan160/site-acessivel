242365303d228f338a91ac8f4cc1df299e61f240
fan160 <167432653+fan160@users.noreply.github.com>
2025-04-04 07:36:49 -0300

    ..

--- script.js
+++ script.js
@@ -11,8 +11,7 @@ document.addEventListener('DOMContentLoaded', function(){
    
     })
  
-     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    let tamanhoAtualFonte = 1;
      
      const alternaContraste = document.getElementById('alterna-contraste')
  
