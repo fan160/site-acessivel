57f21784d9813d4ff55ba90e7b324c886ce2f657
fan160 <167432653+fan160@users.noreply.github.com>
2025-04-04 07:44:52 -0300

    js

--- script2.js
+++ script2.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
- 
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    })
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
     aumentaFonteBotao.addEventListener('click', function(){
-        tamanhoAtualFonte += 0.1;
+        tamanhoAtualFonte += 0.1;botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    })
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
 
@@ -15,3 +21,4 @@ document.addEventListener('DOMContentLoaded', function(){
 
     })
     })
+    
